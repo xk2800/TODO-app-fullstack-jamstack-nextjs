80252bfbe8060fedbf39a77f1621a539deb0e81c
Xavier K <xavierkhew00@gmail.com>
2021-08-30 18:26:55 +0800

    add serverside props

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import Todo from '../components/Todo';
+import { table, getMinifiedRecord } from './api/utils/AirTable';
 
-export default function Home() {
+export default function Home({ initialTodos }) {
+    console.log(initialTodos);
     return (
         <div>
             <Head>
@@ -12,7 +15,28 @@ export default function Home() {
             <Navbar />
             <main>
                 <h1>Todo App</h1>
+                {initialTodos.map(todo =>
+                    <Todo key={todo.id} todo={todo} />
+                    )}
             </main>
         </div>
     );
+}
+
+export async function getServerSideProps(context) {
+    try {
+        const todos = await table.select({}).firstPage();
+        return {
+            props: {
+                initialTodos: getMinifiedRecord(todos)
+            }
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            props: {
+                err: "Something went wrong"
+            }
+        };
+    }
 }
