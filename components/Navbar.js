a8682975e16cdd6d9c72fe6da2fa322e49a57324
Xavier K <xavierkhew00@gmail.com>
2021-08-30 21:53:55 +0800

    add: oauth

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,28 @@
 import Link from 'next/link';
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Navbar() {
+    const [session, loading] = useSession();
     return (
         <nav className="flex justify-between items-center py-4">
             <p className="text-2xl font-bold text-red-500">My Todos</p>
             <div className="flex">
-                <Link href='/api/logout'>
+                {/* <Link href='/api/logout'>
                     <a className="rounded bg-blue-500 hover:bg-blue-700 text-white py-2 px-4">Logout</a>
-                </Link>
+                </Link> */}
+                {!session && (
+                    <>
+                        Not signed in <br />
+                        <button onClick={() => signIn()} className="rounded bg-blue-500 hover:bg-blue-700 text-white py-2 px-4">Sign in</button>
+                    </>
+                )}
 
-                <Link href='/api/login'>
-                    <a className="rounded bg-blue-500 hover:bg-blue-700 text-white py-2 px-4">Login</a>
-                </Link>
+                {session && (
+                    <>
+                        Signed in as {session.user.email} <br />
+                        <button onClick={() => signOut()} className="rounded bg-blue-500 hover:bg-blue-700 text-white py-2 px-4">Sign out</button>
+                    </>
+                )}
             </div>
         </nav>
     );
