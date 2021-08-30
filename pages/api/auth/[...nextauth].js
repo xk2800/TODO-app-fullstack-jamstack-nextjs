ebae8650463444eba4b1aeab6169bf07300cc1b9
Xavier K <xavierkhew00@gmail.com>
2021-08-31 00:52:07 +0800

    remove github from oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,6 @@ import Providers from "next-auth/providers";
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
         // ...add more providers here
         Providers.Google({
             clientId: process.env.GOOGLE_ID,
