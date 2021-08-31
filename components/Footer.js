ead4ecf8f566880997413b5d535b8afb2ae0fab8
Xavier K <xavierkhew00@gmail.com>
2021-08-31 21:59:15 +0800

    add: footer

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <div className="mt-[80px] text-center font-main">
+            The Todo Site,
+            Fruits of Xavier K.
+        </div>
+    )
+}
