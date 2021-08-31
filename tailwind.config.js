392d82c46710ebcb72e2320e38f50221a157b6a5
Xavier K <xavierkhew00@gmail.com>
2021-08-31 19:37:50 +0800

    config custom tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ['Lexend Deca'],
+        secondary: ['Roboto'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
