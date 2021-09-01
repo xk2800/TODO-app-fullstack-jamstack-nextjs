d978f2f763971e179f96f186523212d36ca204b6
Xavier K <xavierkhew00@gmail.com>
2021-09-02 03:52:48 +0800

    add discord oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,11 @@ export default NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
+        Providers.Discord({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        }),
+
     ],
     pages: {
         signIn: "/signin",
