96891ee6f53a056ed07e233f81df5e2c7775fb8d
Xavier K <xavierkhew00@gmail.com>
2021-09-02 03:52:56 +0800

    add: cancel button

--- pages/signin.js
+++ pages/signin.js
@@ -1,27 +1,33 @@
+import Link from 'next/link';
 import { providers, signIn, getSession, csrfToken } from "next-auth/client";
 
 
 export default function SignIn({ providers, csrfToken }) {
     return (
         <>
-            <div className="text-3xl font-main text-[color:var(--darkbrown)] mb-[20px] text-center">
+            <div className="text-3xl font-main text-[color:var(--darkbrown)] mb-[20px] text-center dark:bg-black">
                 Login to continue
             </div>
             <div className="text-xl font-secondary text-center">
                 {/* <button className="border-[2px] border-black px-[10px]"> */}
-                    {Object.values(providers).map((provider) => {
-                        if (provider.name === "Email") {
-                            return;
-                        }
-                        return (
+                {Object.values(providers).map((provider) => {
+                    if (provider.name === "Email") {
+                        return;
+                    }
+                    return (
+                        <>
                             <div key={provider.name}>
                                 <button variant="outline" onClick={() => signIn(provider.id)} className="rounded-[10px] border-[2px] border-[color:var(--darkbrown)] bg-[color:var(--darkbrown)] hover:bg-[color:var(--brown)] hover:border-[color:var(--brown)] hover:text-black text-white px-[15px] py-[20px]">
                                     Login with {provider.name}
                                 </button>
                             </div>
-                        );
-                    })}
+                            <br />
+                            
+                        </>
+                    );
+                })}
                 {/* </button> */}
+                <Link href="/"><button className="rounded-[10px] border-[2px] px-[75px] py-[20px]">Test</button></Link>
             </div>
         </>
     );
@@ -31,7 +37,7 @@ SignIn.getInitialProps = async (context) => {
     const { req, res } = context;
     const session = await getSession({ req });
 
-    if (session && res ) {
+    if (session && res) {
         res.writeHead(302, {
             Location: "/",
         });
