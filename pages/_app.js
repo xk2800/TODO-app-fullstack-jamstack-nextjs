00ba47eb44884cc8c3454a3edef5d6dbb43b3d4b
Restyled.io <commits@restyled.io>
2021-09-01 10:15:20 +0000

    Restyled by clang-format

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,19 @@
 import '../styles/globals.css';
-import { Provider } from "next-auth/client";
-import { TodosProvider } from '../context/TodosContext';
+
+import {Provider} from "next-auth/client";
+
 import Container from '../components/Container';
 import TodoFooter from '../components/Footer';
+import {TodosProvider} from '../context/TodosContext';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <TodosProvider>
-        <div className="dark:bg-black">
-          <Container>
-            <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (<Provider session = {pageProps.session}><TodosProvider>
+          <div className = "dark:bg-black"><Container>
+          <Component { ...pageProps } />
             <TodoFooter />
           </Container>
-        </div>
-      </TodosProvider>
-    </Provider>
-  );
+        </div></TodosProvider>
+    </Provider>);
 }
 
 export default MyApp;
